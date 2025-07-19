2a2ebcb1404f07a6d4edd91965bb3bf14ab3ee5d
gabrieltodua <lika.sichinava@yahoo.com>
2025-07-19 11:49:52 +0400

    Add files via upload

---
+++ day 003/classwork/lesson1.py
@@ -0,0 +1,24 @@
+
+#ქვემოთ მოცემული კოდი დააკოპირეთ და გვერდზე მიუწერეთ, რას გამოიტანს ის:
+#მაგ. print(type(1)) #integer
+#print(type(20))
+#print(type(12))
+#print(type('hello'))
+#print(type('111'))
+#print(type('1.5'))
+#print(type(2.2))
+#print('Group 74')
+#print('Hello world')
+
+
+
+print(type(20))    # int
+print(type(12))     #int
+print(type('hello'))    #str
+print(type('111'))      #str
+print(type('1.5'))      #str
+print(type(2.2))     #float
+print("Group 74")         #str
+print('Hello world')      #str
+
+
---
+++ day005/classwork/lesson2.py
@@ -0,0 +1,18 @@
+
+# 1) მომხმარებელს შემოატანინეთ მშობლის და თავისი სახელი, შემდეგ პრინტში გამოიტანეთ "*მათი სახელი* არის *მშობლის სახელი*ს შვილი"
+# 2)მომხმარებელ შემოატანინეთ ორი რიცხვი და ამ რიცხვებს შორის ჩაატარეთ ყველა ნასწავლი მათემატიკური ოპერაცია(+,-,*,/)
+
+
+name1 = input("your name ")
+name2 = input("your mother' and yor father's name")
+print(name1,"არის",name2 + "ს", "შვილი")
+
+num1 = int(input("Please input a number: "))
+num2 = int(input("Please input a number: "))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
+
