863ed852a7f70b5fc196ae77107c064b9a9b3fc9
gabrieltodua <lika.sichinava@yahoo.com>
2025-07-21 21:07:37 +0400

    Add files via upload

---
+++ day 005/classwork/lesson1.py
@@ -0,0 +1,10 @@
+
+# მომხმარებელს შემოატანინეთ სახელი და ასაკი, შემდეგ პრინტის დახმარებით დაწერეთ "გამარჯობა *მათი სახელი*, თქვენ ხართ *მათი ასაკი* წლის
+print("hollo")
+name = input(' your name' )
+age = input(' your age ')
+print("name"+name)
+print("age"+age)
+print("heloo " + name)
+
+
