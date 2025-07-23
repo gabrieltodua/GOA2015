d08b37a4861b3cc48f578ddbcedb6164c8d00e16
gabrieltodua <lika.sichinava@yahoo.com>
2025-07-23 16:58:17 +0400

    Add files via upload

---
+++ day 007/classwork/lesson2.py
@@ -0,0 +1,25 @@
+
+# 1)კომენტარით დაწერეთ რას გამოიტანს ქვემოთ მოეცემული კოდი:
+# print(True and False or False or True (False and False) or (True and True) or (False or True) or False or True and False)
+# 2)კომენტარებით ჩამოწერეთ პითონში რა თანმიმდევრობით სრულდება: (), or და and
+
+
+print(True and False or False or True (False and False) or (True and True) or (False or True) or False or True and False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
