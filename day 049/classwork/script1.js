ce74804cc68b79e2968f3a072b317eded9b21b05
gabrieltodua <lika.sichinava@yahoo.com>
2025-12-19 15:39:06 +0400

    Add files via upload

---
+++ day 049/classwork/script1.js
@@ -0,0 +1,19 @@
+
+let NAME = "..."
+
+let LASTNAME = "..."
+
+let AGE = "..."
+
+function input(){
+
+    NAME = prompt(" please input your name : ")
+    LASTNAME = prompt(" please input your lastname : ")
+    AGE = prompt(" please input your age : ")
+
+    console.log(NAME)
+
+    console.log(LASTNAME)
+
+    console.log(AGE)
+}
---
+++ day 049/classwork/script2.js
@@ -0,0 +1,12 @@
+
+
+function INPUT(e){
+
+    e.preventDefault();
+
+    let inp = document.getElementById("MAIN")
+
+    console.log(inp.value)
+
+
+}
