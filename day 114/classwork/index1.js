9459ada5bc16c7b15302de307059b1b818d7dd08
gabrieltodua <lika.sichinava@yahoo.com>
2026-08-26 17:33:53 +0400

    Add files via upload

---
+++ day 114/classwork/index1.js
@@ -0,0 +1,61 @@
+// 0
+
+
+
+// 1
+
+// main = [2,3,4,5,6,7,8,9,10,11]
+
+// main.forEach(cur => {
+//     console.log(cur - 1)
+// });
+
+// console.log(main.filter(
+//     cur => cur % 2 === 0
+// ))
+
+
+
+// 2
+
+
+
+// 3
+
+// let numbers = [1, 2, 3, 4, 5];
+
+
+
+// // reduce
+
+// let total = numbers.reduce((sum, cur) => sum + cur, 3);
+// console.log(total);
+
+// // some
+
+// let hasEvenNumber = numbers.some(cur => cur % 2 === 0);
+// console.log(hasEvenNumber);
+
+// // every
+
+// let allAreEven = numbers.every(cur => cur % 2 === 0);
+// console.log(allAreEven);
+
+
+
+// 4
+
+// main1 = [{id : 1 , name : "nika"} , {id : 2 , name : "luka"}]
+
+// find
+
+// let found = main1.find(person => person.id === 1);
+// console.log(found);
+
+// findIndex
+
+// let index = main1.findIndex(person => person.name === "luka");
+// console.log(index);
+
+
+
